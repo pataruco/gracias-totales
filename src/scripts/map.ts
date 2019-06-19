7e5074b663ef27b8a44672bfd0ed6d7766563cba
Pedro Martin <pataruco@gmail.com>
2019-06-20 00:26:25 +0100

    Rendering a map

--- src/scripts/map.ts
+++ src/scripts/map.ts
@@ -1,13 +1,50 @@
-const picture = document.getElementById('js-map') as HTMLPictureElement;
-const img = new Image();
-const width = window.innerWidth;
-const height = window.innerHeight - 20;
+const avalon = { lat: 51.450616, lng: -0.1480473 };
+const mapElement = document.getElementById('js-map') as HTMLDivElement;
 
-const renderMap = (): void => {
-  const src = document.createAttribute('src');
-  src.value = `https://maps.googleapis.com/maps/api/staticmap?center=london&zoom=12&scale=2&size=${width}x${height}&maptype=roadmap&key=AIzaSyBum8HUlhV6EPKJQVzxLeWkNfpHNivlYjc&format=jpg&visual_refresh=true`;
-  img.setAttributeNode(src);
-  picture.appendChild(img);
+const getUserCoordinates = async (): Promise<google.maps.LatLng> => {
+  return new Promise(resolve => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { coords } = position;
+      const { latitude, longitude } = coords;
+      resolve(new google.maps.LatLng(latitude, longitude));
+    });
+  });
+};
+
+const renderMap = async () => {
+  const map = new google.maps.Map(mapElement, {
+    center: avalon,
+    zoom: 15,
+  });
+  let from;
+
+  try {
+    from = await getUserCoordinates();
+  } catch (error) {
+    // tslint:disable-next-line:no-console
+    console.error(error);
+  }
+
+  if (from) {
+    const directionsService = new google.maps.DirectionsService();
+    const directionsDisplay = new google.maps.DirectionsRenderer();
+    directionsDisplay.setMap(map);
+
+    directionsService.route(
+      {
+        origin: from,
+        destination: avalon,
+        // @ts-ignore
+        travelMode: 'DRIVING',
+      },
+      // @ts-ignore
+      (result, status) => {
+        if (status === 'OK') {
+          directionsDisplay.setDirections(result);
+        }
+      },
+    );
+  }
 };
 
 window.addEventListener('load', renderMap);
