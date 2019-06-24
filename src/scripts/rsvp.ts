951ebb08f0d65b522c97b26d0e15e4017546aa29
Pedro Martin <pataruco@gmail.com>
2019-06-24 10:20:02 +0100

    refresh google api token and patch event

--- src/scripts/rsvp.ts
+++ src/scripts/rsvp.ts
@@ -1,47 +1,55 @@
-// import { google } from 'googleapis';
-
 const CLIENT_ID =
   '309718613253-re1s119gre0p5d0d2ir90ktfl347n0he.apps.googleusercontent.com';
 const CLIENT_SECRET = 'OHlBfFNm-JVaQexokYlet3He';
-const calendarId = 'pataruco@gmail.com';
-const eventId = '3je4tu7k6pkav5issgotebmeae';
+const CALENDAR_ID = 'pataruco@gmail.com';
+const EVENT_ID = '3je4tu7k6pkav5issgotebmeae';
+const REFRESH_TOKEN = '1/EGlYKQqAAJ8g6Z3vJweTC3ofS0ANUq-s6o-v4SkBlXs';
 
-const token = {
-  access_token:
-    'ya29.GlsvB16U4FgD-NsgcIAJJs6nwmQP4CDtzpyY1P5KPWEDeIYx0QDfTS5y0NCAEDiTIDzaMACxJDcdTCCuvFvwjo-q4jImYiLjnGZEAmj58pG8SnZ0VmI3HLb0Ak1G',
-  expires_in: 3600,
-  refresh_token: '1/EGlYKQqAAJ8g6Z3vJweTC3ofS0ANUq-s6o-v4SkBlXs',
-  scope: 'https://www.googleapis.com/auth/calendar.events',
-  token_type: 'Bearer',
+const refreshTokenUrl = `https://www.googleapis.com/oauth2/v4/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&refresh_token=${REFRESH_TOKEN}&grant_type=refresh_token`;
+
+const getToken = async () => {
+  const response = await fetch(refreshTokenUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+
+  const data = await response.json();
+  const { access_token } = data;
+  return access_token;
 };
 
 const getEvent = async () => {
+  const accessToken = await getToken();
   const response = await fetch(
-    `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events/${eventId}`,
+    `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events/${EVENT_ID}`,
     {
       method: 'GET',
       headers: {
-        Authorization: `Bearer ${token.access_token}`,
+        Authorization: `Bearer ${accessToken}`,
         Accept: 'application/json',
       },
     },
   );
-
   return response.json();
 };
 
 const updateEvent = async (email: string) => {
+  const accessToken = await getToken();
   const event = await getEvent();
   const { attendees } = event;
   const attendee = { email };
+
   attendees.push(attendee);
   const response = await fetch(
-    `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events/${eventId}`,
+    `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events/${EVENT_ID}`,
     {
       method: 'PATCH',
       headers: {
-        Authorization: `Bearer ${token.access_token}`,
+        Authorization: `Bearer ${accessToken}`,
         Accept: 'application/json',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         attendees,
@@ -50,7 +58,7 @@ const updateEvent = async (email: string) => {
   );
 
   const data = await response.json();
-  // console.log({ response, data, attendees });
+  return data;
 };
 
-updateEvent('pataruco+test@gmail.com');
+updateEvent('pedro.martin@red-badger.com');
