b1fbff4191ab6a7baae74f51aa1c576c4ef2ce57
Pedro Martin <pataruco@gmail.com>
2019-06-25 00:43:41 +0100

    Add path to css

--- config/webpack.config.prod.ts
+++ config/webpack.config.prod.ts
@@ -36,7 +36,7 @@ const config: webpack.Configuration = {
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
-      chunkFilename: '[name].css',
+      chunkFilename: './[name].css',
     }),
   ],
   node: {
@@ -45,7 +45,7 @@ const config: webpack.Configuration = {
   optimization: {
     minimizer: [
       new TerserJSPlugin({ extractComments: true }),
-      // new OptimizeCSSAssetsPlugin({}),
+      new OptimizeCSSAssetsPlugin({}),
     ],
   },
 };
