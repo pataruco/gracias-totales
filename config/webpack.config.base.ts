717e52f8981da3bc175151c1a65b5e2cdc44079c
Pedro Martin <pataruco@gmail.com>
2021-06-14 15:31:54 +0100

    Set a webpack base

---
+++ config/webpack.config.base.ts
@@ -0,0 +1,66 @@
+import FaviconsWebpackPlugin from 'favicons-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import path from 'path';
+import webpack from 'webpack';
+import dotenv from 'dotenv';
+
+dotenv.config;
+
+const { CALENDAR_ID, CLIENT_ID, CLIENT_SECRET, EVENT_ID, REFRESH_TOKEN } =
+  process.env;
+
+export const favicon = path.resolve(
+  __dirname,
+  '../src/assets/party-popper.png',
+);
+
+const plugins: webpack.Configuration['plugins'] = [
+  new HtmlWebpackPlugin({
+    title: '¡Gracias totales!',
+    filename: 'index.html',
+    template: 'src/index.html',
+  }),
+  new MiniCssExtractPlugin({
+    filename: '[name].css',
+    chunkFilename: '[name].css',
+  }),
+  new FaviconsWebpackPlugin(favicon),
+  new webpack.DefinePlugin({
+    envs: JSON.stringify({
+      CALENDAR_ID,
+      CLIENT_ID,
+      CLIENT_SECRET,
+      EVENT_ID,
+      REFRESH_TOKEN,
+    }),
+  }),
+];
+
+const config: webpack.Configuration = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      { test: /\.ts$/, exclude: /node_modules/, loader: 'ts-loader' },
+      {
+        test: /\.svg$/,
+        loader: 'svg-url-loader',
+      },
+    ],
+  },
+  plugins,
+};
+
+export default config;
--- config/webpack.config.dev.ts
+++ config/webpack.config.dev.ts
@@ -1,66 +1,17 @@
-import FaviconsWebpackPlugin from 'favicons-webpack-plugin';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import path from 'path';
 import webpack from 'webpack';
-import dotenv from 'dotenv';
 
-dotenv.config;
+import config from './webpack.config.base';
 
-const { CALENDAR_ID, CLIENT_ID, CLIENT_SECRET, EVENT_ID, REFRESH_TOKEN } =
-  process.env;
-
-export const favicon = path.resolve(
-  __dirname,
-  '../src/assets/party-popper.png',
+const plugins = config.plugins?.concat(
+  new webpack.HotModuleReplacementPlugin(),
 );
 
-const config: webpack.Configuration = {
-  entry: './src/index.ts',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, '../dist'),
-  },
+const devConfig: webpack.Configuration = {
+  ...config,
   devtool: 'inline-source-map',
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
   mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      { test: /\.ts$/, exclude: /node_modules/, loader: 'ts-loader' },
-      {
-        test: /\.svg$/,
-        loader: 'svg-url-loader',
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: '¡Gracias totales!',
-      filename: 'index.html',
-      template: 'src/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[name].css',
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new FaviconsWebpackPlugin(favicon),
-    new webpack.DefinePlugin({
-      envs: JSON.stringify({
-        CALENDAR_ID,
-        CLIENT_ID,
-        CLIENT_SECRET,
-        EVENT_ID,
-        REFRESH_TOKEN,
-      }),
-    }),
-  ],
+  plugins,
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     port: 3000,
@@ -70,4 +21,4 @@ const config: webpack.Configuration = {
   },
 };
 
-export default config;
+export default devConfig;
