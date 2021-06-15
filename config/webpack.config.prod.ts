30e1aa95078678a0cd9db1b729542ef2853bf730
Pedro Martin <pataruco@gmail.com>
2021-06-15 11:27:49 +0100

    Remove not used dependencies

--- config/webpack.config.prod.ts
+++ config/webpack.config.prod.ts
@@ -1,9 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
-import path from 'path';
-import TerserJSPlugin from 'terser-webpack-plugin';
 import webpack from 'webpack';
 
 import config from './webpack.config.base';
